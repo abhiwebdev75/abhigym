39764ead099a16f09d5bf17f6fee491fd738946b
abhiwebdev75 <130377672+abhiwebdev75@users.noreply.github.com>
2023-04-10 19:57:56 +0530

    Add files via upload

---
+++ js/gym.js
@@ -0,0 +1,14 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+  function on() {
+    document.getElementById("overlay").style.display = "block";;
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
